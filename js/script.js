0e991e92fed8a7abea2d57542c5af3255447c589
kelsie_b <kelsie.p.brown@gmail.com>
2024-01-26 22:30:43 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const menuButton = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('is-active');
+    navMenu.classList.toggle('active');
+    
+});
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    menuButton.classList.remove('is-active');
+    navMenu.classList.remove('active');
+}));
