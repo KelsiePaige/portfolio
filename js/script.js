63381350397d01110bf9fcbb9a17765c6ae43c03
kelsie_b <kelsie.p.brown@gmail.com>
2024-03-26 16:21:03 -0400

    Removed white space

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ const navMenu = document.querySelector('.nav-menu');
 
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('is-active');
-    navMenu.classList.toggle('active');
-    
+    navMenu.classList.toggle('active');    
 });
 
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
     menuButton.classList.remove('is-active');
     navMenu.classList.remove('active');
 }));
+
